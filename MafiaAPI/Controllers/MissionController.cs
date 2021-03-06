4225bee7481f0b44d4c93451200281cc1d05ddc6
Patryk Margasinski <patrykmargasinski@gmail.com>
2021-03-06 18:06:21 +0100

    Added mission model and controller (get, post, put, delete endpoints)

---
+++ MafiaAPI/Controllers/MissionController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MissionController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public MissionController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+            select MissionID, MissionName, DifficultyLevel, Loot from dbo.Mission";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MafiaAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Mission mis)
+        {
+            string query = @"
+            insert into dbo.Mission values ('" + 
+            mis.MissionName + "', " + 
+            mis.DifficultyLevel + ", " + 
+            mis.Loot+ 
+            @")";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MafiaAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Update(Mission mis)
+        {
+            string query = @"
+            update dbo.Mission set
+            MissionName ='" + mis.MissionName + @"',
+            DifficultyLevel="+mis.DifficultyLevel+@",
+            Loot = "+mis.Loot+@"
+            where MissionId=" + mis.MissionId;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MafiaAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated successfully");
+        }
+
+        [Route("[controller]/id")]
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+            delete from dbo.Mission where MissionId=" + id;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("MafiaAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted successfully");
+        }
+    }
+}
---
+++ MafiaAPI/Models/Mission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Mission
+    {
+        public int MissionId { get; set; }
+        public string MissionName { get; set; }
+        public int DifficultyLevel { get; set; }
+        public int Loot { get; set; }
+    }
+}

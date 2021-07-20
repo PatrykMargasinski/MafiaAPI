a2a88d976214d88d1784a03d54b09c1259c571f2
unknown <krzysztofmichalski987@gmail.com>
2021-07-20 13:30:59 +0200

    db migration

---
+++ MafiaAPI/Migrations/20210720112928_add_dex_int_to_agent.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MafiaAPI.Migrations
+{
+    public partial class add_dex_int_to_agent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Strength",
+                table: "Agent",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Dexterity",
+                table: "Agent",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Intelligence",
+                table: "Agent",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Boss",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "LastSeen",
+                value: new DateTime(2021, 7, 20, 13, 29, 27, 979, DateTimeKind.Local).AddTicks(7612));
+
+            migrationBuilder.UpdateData(
+                table: "Boss",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "LastSeen",
+                value: new DateTime(2021, 7, 20, 13, 29, 27, 989, DateTimeKind.Local).AddTicks(1398));
+
+            migrationBuilder.UpdateData(
+                table: "PerformingMission",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CompletionTime",
+                value: new DateTime(2021, 7, 20, 13, 29, 27, 993, DateTimeKind.Local).AddTicks(8511));
+
+            migrationBuilder.UpdateData(
+                table: "PerformingMission",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CompletionTime",
+                value: new DateTime(2021, 7, 20, 13, 29, 27, 993, DateTimeKind.Local).AddTicks(9128));
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Password",
+                value: "tlnK6HiwFF4+b4DRVaVdRlIPtzduirsf8W3+nbXlLWlf9c/J");
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Password",
+                value: "d2JZt0Jz9UzgW1l544W2WnOaX14u/pfGUDYTQzv5AEWk3W7D");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dexterity",
+                table: "Agent");
+
+            migrationBuilder.DropColumn(
+                name: "Intelligence",
+                table: "Agent");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Strength",
+                table: "Agent",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Boss",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "LastSeen",
+                value: new DateTime(2021, 7, 12, 17, 32, 30, 950, DateTimeKind.Local).AddTicks(7202));
+
+            migrationBuilder.UpdateData(
+                table: "Boss",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "LastSeen",
+                value: new DateTime(2021, 7, 12, 17, 32, 30, 954, DateTimeKind.Local).AddTicks(3150));
+
+            migrationBuilder.UpdateData(
+                table: "PerformingMission",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CompletionTime",
+                value: new DateTime(2021, 7, 12, 17, 32, 30, 959, DateTimeKind.Local).AddTicks(1174));
+
+            migrationBuilder.UpdateData(
+                table: "PerformingMission",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "CompletionTime",
+                value: new DateTime(2021, 7, 12, 17, 32, 30, 959, DateTimeKind.Local).AddTicks(1928));
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Password",
+                value: "a");
+
+            migrationBuilder.UpdateData(
+                table: "Player",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Password",
+                value: "b");
+        }
+    }
+}
